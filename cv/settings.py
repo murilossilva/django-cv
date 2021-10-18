74b04b54b2898f7a565fdb9316581eafe0633ce8
Murilo Souza <82983123+murilossilva@users.noreply.github.com>
2021-10-18 17:01:41 -0300

    Update settings.py

--- cv/settings.py
+++ cv/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
 from decouple import config
 
@@ -23,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^z5ql7ko5vr*j2beu!@&5$6ns0-l3t$mfo)jro*%2eneu^8)4^'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
